bcdf7b6f97b797ba001197f45229657c95f45e1c
Tankdestroyer212 <dhruvkagatimath@gmail.com>
2026-03-23 23:04:32 -0700

    Fixed the issues with the check win and how it was swapped. Also added the "QUARTO!" for the win condition at the end.

--- Assets/QuartoGameState.cs
+++ Assets/QuartoGameState.cs
@@ -52,7 +52,7 @@ public class GameState : MonoBehaviour
     {
         if (piece == null || square == null) return false;
 
-        // gets the board pos
+        // gets the board position
         BoardSquare squareComponent = square.GetComponent<BoardSquare>();
         if (squareComponent == null) return false;
 
@@ -67,7 +67,18 @@ public class GameState : MonoBehaviour
         boardPieces[row, col] = piece;
         unplacedPieces.Remove(piece);
 
-        print(CheckRowForWin(row));
+        bool didWin = false;
+        
+        if (CheckRowForWin(row)) didWin = true;
+        if (CheckColumnForWin(col)) didWin = true;
+        
+        if (row == col && CheckDiagonalForWin(0)) didWin = true;
+        if (row + col == 3 && CheckDiagonalForWin(1)) didWin = true;
+
+        if (didWin)
+        {
+            Debug.Log("QUARTO!");
+        }
 
         return true;
     }
@@ -77,10 +88,10 @@ public class GameState : MonoBehaviour
         if (row < 0 || row > 3) return false;
         
         return CheckLineForWin(
-            boardPieces[0, row],
-            boardPieces[1, row],
-            boardPieces[2, row],
-            boardPieces[3, row]
+            boardPieces[row, 0],
+            boardPieces[row, 1],
+            boardPieces[row, 2],
+            boardPieces[row, 3]
         );
     }
 
